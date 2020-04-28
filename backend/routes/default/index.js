d1b26db57765e1269e1fc5a4c0eaa1f50e0d0eb0
srinskit <srinskit@users.noreply.github.com>
2020-04-28 16:43:41 +0530

    Added webapp build delivery

--- backend/routes/default/index.js
+++ backend/routes/default/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
-router.get('/', function (req, res) {
-	res.sendStatus(200);
-});
+router.use('/', express.static(path.join(__dirname + '/build')));
+router.use('/', (req, res) => res.sendFile(path.join(__dirname + '/build/index.html')));
 
 module.exports = router;
