7dc329409163fc8b05f0560675ba9664e2efb84b
srinskit <srinskit@users.noreply.github.com>
2020-05-06 13:17:35 +0530

    Limited scope of CORS

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ var defaultRouter = require('./routes/default');
 
 var app = express();
 app.use(logger('dev'));
-app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -32,7 +31,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/api', apiRouter);
+app.use('/api', cors, apiRouter);
 app.use('/', defaultRouter);
 app.use(function (req, res) {
 	res.sendStatus(httpStatus.NOT_FOUND);
