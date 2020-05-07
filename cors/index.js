c518831f25c5daca9b28c576feca53187748c24b
srinskit <srinskit@users.noreply.github.com>
2020-05-07 18:27:29 +0530

    CORS now supports url exceptions

--- cors/index.js
+++ cors/index.js
@@ -5,13 +5,36 @@ const allowedOrigins = [
 	'http://localhost:5000',
 ];
 
+const exceptionRoutes = [
+	'/v1/spotify/auth/authorize',
+	'/v1/spotify/auth/callback',
+];
+
+const allowedMethods = [
+	'GET', 'PUT', 'POST', 'DELETE', 'PATCH', 'OPTIONS'
+].join(',');
+
+const allowedHeaders = [
+	'X-Requested-With', 'X-HTTP-Method-Override',
+	'Content-Type', 'Accept', 'credentials', 'cache-control',
+].join(',');
+
+function isException(url, exceptionRoutes) {
+	for (let i = 0; i < exceptionRoutes.length; ++i) {
+		if (url.startsWith(exceptionRoutes[i])) {
+			return true;
+		}
+	}
+	return false;
+}
+
 function cors(req, res, next) {
 	const origin = req.headers.origin;
-	if (allowedOrigins.indexOf(origin) >= 0) {
+	if (allowedOrigins.indexOf(origin) >= 0 || isException(req.url, exceptionRoutes)) {
 		res.header('Access-Control-Allow-Credentials', true);
 		res.header('Access-Control-Allow-Origin', origin);
-		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-		res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization, credentials');
+		res.header('Access-Control-Allow-Methods', allowedMethods);
+		res.header('Access-Control-Allow-Headers', allowedHeaders);
 		if ('OPTIONS' === req.method) {
 			res.sendStatus(httpStatus.OK);
 		} else {
@@ -24,4 +47,4 @@ function cors(req, res, next) {
 }
 
 
-module.exports = cors ;
+module.exports = cors;
